0d9012596d44800ec4aea233b1d5e3dc700fac9d
simonjujuh <juhel.simon6@gmail.com>
2023-09-05 23:31:15 +0200

    Improve and comment main script

--- scripts/audit-new.py
+++ scripts/audit-new.py
@@ -2,6 +2,7 @@
 # -*- encoding: utf-8 -*-
 
 
+# Imports section
 import os
 import sys
 import pathlib
@@ -9,41 +10,52 @@ import argparse
 import configparser
 
 
-config_path = pathlib.Path('~/.local/share/audit-tools/config.ini').expanduser()
-projects_base_dir = None
-templates = {}
+def load_config(file_path):
+    """Load the audits configuration parameters
 
-def load_config():
-    global projects_base_dir
-    global templates
+    Returns a dictionnary the configurations settings
+    """
+
+    # Convert file_path parameter to a Path object
+    file_path = pathlib.Path(file_path).expanduser()
 
     # Check if the configuration file exists
-    if not config_path.exists():
-        print(f"[-] Unable to find config file {config_path}")
+    if not file_path.exists():
+        print(f"[-] Unable to find config file {file_path}")
         print(f"[*] Please run the `install.py` script in this project")
         sys.exit(1)
     
-    # Read and load the configuration
+    # Create the config parser object
+    config = configparser.ConfigParser()
+    
     try:
-        config = configparser.ConfigParser()
-        config.read(config_path)
+        # Read and load the configuration
+        config.read(file_path)
     except Exception as e:
         print(f"[-] Error reading configuration file: {e}")
         sys.exit(1)
-
-    # load the base directory for audits projects
-    projects_base_dir = pathlib.Path(config.get('audits', 'directory')).expanduser()
-    # load the available projects templates
+    
+    # Load the available projects templates
+    templates = {}
     for template_name in config['templates']:
         template_dirtree_str = config['templates'][template_name]
         template_dirtree_lst = [x.strip() for x in template_dirtree_str.split(',')]
         templates.update({template_name: template_dirtree_lst})
 
+    # Define the configuration dictionnary to retun
+    config = {
+        "projects_dir": pathlib.Path(config.get('audits', 'directory')).expanduser(),
+        "templates": templates,
+    }
+
+    return config
+
 
 def main():
     """Main function"""
-    load_config()
-    template_names = list(templates.keys())
+    
+    # Load the configuration from file
+    config = load_config('~/.local/share/audit-tools/config.ini')
 
     # Command line parsing
     parser = argparse.ArgumentParser()
@@ -52,38 +64,44 @@ def main():
         help="Create a symlink to the newly created project")
     parser.add_argument('-t', '--template', action='store',
         help=f"Choose a directory structure for the project, \
-        based on templates (available are: {template_names})")
+        based on templates (available are: {list(config['templates'].keys())})")
     parser.add_argument('name', help="The audit project's name")
 
     args = parser.parse_args()
 
     # Create the audit directory
     try:
-        new_project_path = os.path.join(projects_base_dir, args.name)
-        pathlib.Path(new_project_path).mkdir(parents=True)
+        project_path = os.path.join(config['projects_dir'], args.name)
+        pathlib.Path(project_path).mkdir(parents=True)
+        print(f"[+] Directory {project_path} created successfully")
     except Exception as e:
         print(e)
         return 1
 
+    # --template option is set
+    if args.template:
+        # Check if submitted template exists
+        if args.template in config['templates'].keys():
+            # For each directory from the template list, create the new dir
+            for directory in config['templates'][args.template]:
+                new_path = pathlib.Path(project_path) / pathlib.Path(directory)
+                new_path.mkdir()
+            dir_str = ",".join(config['templates'][args.template])
+            print(f"    - {dir_str} created successfully")
+        else:
+            print(f"[!] Unknown template: {args.template} - delete previously created directories")
+            # Delete the project because use might want to reuse same names
+            pathlib.Path(project_path).rmdir()
+            sys.exit(1)
+
+    # --link option is set
     if args.link:
         # Try creating the symlink
         if pathlib.Path(args.link).exists():
-            print(f"[-] Link '{args.link}' already exists")
-            pathlib.Path(new_project_path).rmdir()
-            sys.exit(1)
+            print(f"[!] Link '{args.link}' already exists")
         else:
-            pathlib.Path(args.link).symlink_to(new_project_path, target_is_directory=True)
+            pathlib.Path(args.link).symlink_to(project_path, target_is_directory=True)
             print(f"[+] Link '{args.link}' created successfully")
-
-    if args.template:
-        if args.template in template_names:
-            for directory in templates[args.template]:
-                new_path = pathlib.Path(new_project_path) / pathlib.Path(directory)
-                new_path.mkdir()
-        else:
-            print(f"[-] Unknown template: {args.template}")
-            pathlib.Path(new_project_path).rmdir()
-            sys.exit(1)
 
 
 if __name__ == '__main__':
